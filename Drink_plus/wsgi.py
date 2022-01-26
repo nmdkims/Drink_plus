374608c92afa914d328a0fd5cba7258d4572d09f
KIMHOONHEE <nmdkims@gmail.com>
2022-01-26 22:23:10 +0900

    수정 - folder 이름 수정disneyplus... -> Drink_plus #6

--- Drink_plus/wsgi.py
+++ Drink_plus/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for disneyPlusCloneCoding project.
+WSGI config for Drink_plus project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'disneyPlusCloneCoding.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Drink_plus.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'disneyPlusCloneCoding.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Drink_plus.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
